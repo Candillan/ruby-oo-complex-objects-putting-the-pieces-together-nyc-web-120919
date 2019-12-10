598e62e10c0f8b6222d9ba8d46ebb7b9f04ed19c
Calvin Baker <calvin.baker128@gmail.com>
2019-12-10 18:39:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   def initialize(title, author = nil, page_count = nil, genre = nil)
     @title = title
     @author = author
-    @page = page
+    @page_count = page_count
     @genre = genre
   end
   
